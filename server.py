75d9b6ce9fa82926fa8e61c4723b5d477d9b7cc2
Robert Gustafsson <robg@student.chalmers.se>
2018-03-22 16:35:20 +0100

    Add configfile example for server

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import zmq.asyncio
 import asyncio
 import struct
 import sys
+import configparser
 from channel.ppProtocol import PingPongMessage
 from channel.GossipProtocol import GossipMessage
 from channel.serverRecvChannel import ServerRecvChannel
@@ -20,20 +21,27 @@ class GossipAlg:
     async def callback(self):
         print("Gossip CALLBACK")
 
-port = sys.argv[1]
 p = ppAlg()
 g = GossipAlg()
 gossip_msg = GossipMessage()
 pingTX= gossip_msg.create_message(b'tag_tuple', b'prp',
                             b'msg_all', b'echo', b'uid')
-if(port == "5556"):
-    c1 = SenderChannel(1, g, "127.0.0.1", "5555", pingTX)
-else:
-    c1 = SenderChannel(1, g, "127.0.0.1", "5556", pingTX)
 
-s = ServerRecvChannel(p, g, port)
+my_port = sys.argv[1]
+my_ip = sys.argv[2]
+
 loop = asyncio.get_event_loop()
-loop.create_task(c1.start())
+
+config = configparser.ConfigParser()
+config.read('config/config.ini')
+for key in config['Nodes']:
+    ip, port = config['Nodes'][key].split(':')
+    if not ((ip == my_ip) and (port == my_port)):
+        c = SenderChannel(1, g, ip, port, pingTX)
+        loop.create_task(c.start())
+
+s = ServerRecvChannel(p, g, my_port)
 loop.create_task(s.receive())
+
 loop.run_forever()
 loop.close()
