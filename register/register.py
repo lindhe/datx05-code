7f2ab41a69f8d95c449b74d91fcc09f61db1b1a0
Andreas Lindhé <andreas@lindhe.io>
2018-03-23 11:17:41 +0100

    Change docstrings to adhere better to convention

--- register/register.py
+++ register/register.py
@@ -15,13 +15,11 @@ class Register:
     self.register = {}
 
   def __repr__(self):
-    """ String representation prints the entire dict register """
+    """ String representation prints the entire register dict """
     return str(self.register)
 
-  """
-  Update the set of stored records appropriately
-  """
   def update_phase(self, tag, element, phase):
+    """ Update the set of stored records appropriately """
     S = self.register
     if (tag in S) and S[tag].element and element:
       w = S[tag].element
@@ -31,30 +29,25 @@ class Register:
     else:
       S[tag] = Record(tag, element, phase)
 
-  """
-  Returns the correct phase:
-  pre -> fin
-  fin -> FIN
-  FIN -> FIN
-  """
   def upgrade_phase(self, old, new):
-    try:
-      if (old == 'pre' and new == 'fin'):
-        return 'fin'
-      elif (old == 'fin' and new == 'FIN'):
-        return 'FIN'
-      else:
-        return old
-    except Exception as e:
-      print(type(e))
-      print("Error in upgrade_phase.")
-      print(e)
+    """
+    Returns the correct phase:
+    pre -> fin
+    fin -> FIN
+    FIN -> FIN
+    """
+    if (old == 'pre' and new == 'fin'):
+      return 'fin'
+    elif (old == 'fin' and new == 'FIN'):
+      return 'FIN'
+    else:
+      return old
 
-  """
-  returns the maximum t of all (t, *, p) recrods stored which has p=phase.
-  Argument phases is a list of all phases which should be examined
-  """
   def max_phase(self, phases):
+    """
+    returns the maximum t of all (t, *, p) recrods stored which has p=phase.
+    Argument phases is a list of all phases which should be examined
+    """
     S = self.register
     phase_tags = []
     for tag in S:
@@ -62,10 +55,8 @@ class Register:
         phase_tags.append(tag)
     return max(phase_tags)
 
-  """
-  return the tripple with max_phase of pre, fin and FIN records respectively
-  """
   def tag_tuple(self):
+    """ return the tripple with max_phase of pre, fin and FIN records respectively """
     max_all = self.max_phase(['pre', 'fin', 'FIN'])
     max_finFIN = self.max_phase(['fin', 'FIN'])
     max_FIN = self.max_phase(['FIN'])
