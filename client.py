fde4ca5337ea66f676f19535d8ff5d66ca20359f
Robert Gustafsson <robg@student.chalmers.se>
2018-04-04 09:18:52 +0200

    Add handling of non decoded message

--- client.py
+++ client.py
@@ -12,11 +12,12 @@ from channel.ppProtocol import PingPongMessage
 from pyeclib.ec_iface import ECDriver
 
 class Client:
+
     def __init__(self):
         with open('/sys/class/net/lo/address') as f:
-            self.hw_addr = f.read().splitlines()[0]
-            if (self.hw_addr == '00:00:00:00:00:00'):
-                self.hw_addr = ':'.join(['%02x']*6) % (
+            hw_addr = f.read().splitlines()[0]
+            if (hw_addr == '00:00:00:00:00:00'):
+                hw_addr = ':'.join(['%02x']*6) % (
                             r.randint(0, 255),
                             r.randint(0, 255),
                             r.randint(0, 255),
@@ -25,13 +26,13 @@ class Client:
                             r.randint(0, 255)
                             )
 
+        self.uid = (1, hw_addr)
         self.loop = asyncio.get_event_loop()
         self.ec_driver = ECDriver(k=2, m=2, ec_type='liberasurecode_rs_vand')
         self.p = QuorumSend()
         t = Thread(target=self.start_event_loop, args=(self.loop, 'config/config.ini'))
         t.daemon = True
         t.start()
-
 
     def start_event_loop(self, loop, config_path):
         asyncio.set_event_loop(loop)
@@ -54,26 +55,29 @@ class Client:
         res = self.qrmAccess((None, None, 'qry', 'write'))
         max_tag = max([x.get_tag() for x in res])
         new_int = int(max_tag[0])+1
-        new_tag = (new_int, self.hw_addr)
+        new_tag = (new_int, self.uid)
         self.qrmAccess((new_tag, elements, 'pre', 'write'))
         self.qrmAccess((new_tag, None, 'fin', 'write'))
         self.qrmAccess((new_tag, None, 'FIN', 'write'))
-        print("FINISHED WRITING")
 
     def read(self):
         res = self.qrmAccess((None, None, 'qry', 'read'))
         max_tag = max([x.get_tag() for x in res])
-        print("MAX_TAG %s" % str(max_tag))
         res = self.qrmAccess((max_tag, None, 'fin', 'read'))
         elements = [x.get_data() for x in res]
-        decoded_msg = self.ec_driver.decode(elements)
-        print("RESPONSE TO CLIENT %s" % str(decoded_msg))
+        try:
+            decoded_msg = self.ec_driver.decode(elements)
+        except:
+            decoded_msg = None
+        return decoded_msg
 
 c = Client()
 time.sleep(10)
 c.write(b'hello world')
-c.read()
+s = c.read()
+print("CLIENT: %s" % s)
 time.sleep(10)
 c.write(b'(update) hello world')
-c.read()
+s = c.read()
+print("CLIENT: %s" % s)
 time.sleep(10)
