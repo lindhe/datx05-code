4db30e936dbc16ea2be3ba08e1d9cd3af58f4031
Robert Gustafsson <robg@student.chalmers.se>
2018-04-10 10:08:21 +0200

    Add test case for functionality

---
+++ test/test_functionality.py
@@ -0,0 +1,20 @@
+import unittest
+import struct
+import os
+from client import Client
+
+class TestFunctionality(unittest.TestCase):
+    
+    def setUp(self):
+        self.c = Client()
+        self.c.read()
+
+    def test_read_write(self):
+        for i in range(50): 
+            expected = os.urandom(1024)
+            self.c.write(expected)
+            actual = self.c.read()
+            self.assertEqual(expected, actual)
+
+if __name__ == '__main__':
+    unittest.main()
