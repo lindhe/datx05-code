b8121d8f5ff2360effe38fe8c1b1af2558cdcfe8
Robert Gustafsson <robg@student.chalmers.se>
2018-04-10 10:41:44 +0200

    Refactoring in config script

--- config/create.py
+++ config/create.py
@@ -2,26 +2,30 @@ import sys
 import math
 import configparser
 
-filename = input('Filename: ') + '.ini'
-nbr_of_servers = int(input('Number of servers: '))
-f = int(input('Number of crash prone servers: '))
-k = nbr_of_servers - 2*f
-if(k < 1):
-    raise Exception("Coded elements less than 1")
-config = configparser.ConfigParser()
-config['Nodes'] = {}
-base_port = 5550
-for i in range(nbr_of_servers):
-    nodename = 'Node{}'.format(i)
-    port = base_port + i
-    config['Nodes'][nodename] = '127.0.0.1:{}'.format(port)
+def main(filename, nbr_of_servers, f):
+    k = nbr_of_servers - 2*f
+    if(k < 1):
+        raise Exception("Coded elements less than 1")
+    config = configparser.ConfigParser()
+    config['Nodes'] = {}
+    base_port = 5550
+    for i in range(nbr_of_servers):
+        nodename = 'Node{}'.format(i)
+        port = base_port + i
+        config['Nodes'][nodename] = '127.0.0.1:{}'.format(port)
 
-config['General'] = {}
-config['General']['n'] = str(nbr_of_servers)
-config['General']['f'] = str(f)
-config['General']['e'] = '0'
-config['General']['storage_location'] = './.storage/'
-config['General']['storage_size'] = '10'
+    config['General'] = {}
+    config['General']['n'] = str(nbr_of_servers)
+    config['General']['f'] = str(f)
+    config['General']['e'] = '0'
+    config['General']['storage_location'] = './.storage/'
+    config['General']['storage_size'] = '10'
 
-with open(filename, 'w') as cfgfile:
-    config.write(cfgfile)
+    with open(filename, 'w') as cfgfile:
+        config.write(cfgfile)
+
+if __name__ == '__main__':
+    filename = input('Filename: ') + '.ini'
+    nbr_of_servers = int(input('Number of servers: '))
+    f = int(input('Number of crash prone servers: '))
+    main(filename, nbr_of_servers, f)
