3b457f8c0ef06b59ec93d6b0eae4ad53791601e2
Andreas Lindhé <andreas@lindhe.io>
2018-05-16 14:44:17 +0200

    Make copy skip duplicate servers

--- evaluation/planetlab/copy_to_servers.sh
+++ evaluation/planetlab/copy_to_servers.sh
@@ -2,17 +2,16 @@
 
 ssh_key=~/.ssh/planetlab_rsa
 slice=chalmersple_casss2
-servers=./config/servers.txt
-
+servers=$(cat ./config/servers.txt | sort | uniq)
 failed=0
 
 echo "Copying working directory to servers..."
 
-while read server; do
+for server in $servers; do
   rsync -aPz -e "ssh -i $ssh_key -l $slice" ./* $server:~/casss \
     && echo "Transfer to $server sucessful!"  \
     || { echo "Failed transfer to $server"; failed=1; break; }
-done <$servers
+done
 
 if [ $failed -eq 0 ]; then
   echo "Transfer sucessful!"
