a0840f2ac7b5aaf6085a0f17fe3c061e0f79d5cc
Andreas Lindhé <andreas@lindhe.io>
2018-05-17 16:06:01 +0200

    Add waiting prompt before installing servers

--- evaluation/planetlab/servers_install.sh
+++ evaluation/planetlab/servers_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Remember to update config/servers.txt!"
+read -n 1 -p "Remember to update config/servers.txt! Press enter to continue..."
 ./evaluation/planetlab/setup/create_config.sh
 ./evaluation/planetlab/setup/copy_to_servers.sh
 ./evaluation/planetlab/setup/install.sh
