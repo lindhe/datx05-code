ed719b4b22c40ae0356a6c9dfbaee2ba2483fab0
Andreas Lindhé <andreas@lindhe.io>
2018-05-18 09:13:41 +0200

    Merge client/server's create_config and copy_to_pl

--- evaluation/planetlab/servers_install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-read -n 1 -p "Remember to update config/servers.txt! Press enter to continue..."
-./evaluation/planetlab/setup/create_config.sh
-./evaluation/planetlab/setup/copy_to_servers.sh
-./evaluation/planetlab/setup/install.sh
--- evaluation/planetlab/setup/copy_to_planetlab.sh
+++ evaluation/planetlab/setup/copy_to_planetlab.sh
@@ -3,6 +3,7 @@
 ssh_key=~/.ssh/planetlab_rsa
 slice=chalmersple_casss2
 servers=$(cat ./config/servers.txt | sort | uniq)
+clients=$(cat ./config/clients.txt | sort | uniq)
 failed=0
 
 echo "Copying working directory to servers..."
@@ -12,6 +13,14 @@ for server in $servers; do
     && echo "Transfer to $server sucessful!"  \
     || { echo "Failed transfer to $server"; failed=1; break; }
 done
+
+if [ $failed -eq 0 ]; then
+  for client in $clients; do
+    rsync -aPz -e "ssh -i $ssh_key -l $slice" ./* $client:~/casss \
+      && echo "Transfer to $client sucessful!"  \
+      || { echo "Failed transfer to $client"; failed=1; break; }
+  done
+fi
 
 if [ $failed -eq 0 ]; then
   echo "Transfer sucessful!"
