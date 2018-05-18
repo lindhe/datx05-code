c002f8a25cdde24bc65825a270432826cf092ed1
Andreas Lindhé <andreas@lindhe.io>
2018-05-18 09:34:56 +0200

    Install both clients and servers

--- evaluation/planetlab/install_system.sh
+++ evaluation/planetlab/install_system.sh
@@ -3,3 +3,4 @@
 read -n 1 -p "Remember to update servers.txt and clients.txt! Press enter to continue..."
 ./evaluation/planetlab/setup/create_config.sh
 ./evaluation/planetlab/setup/copy_to_planetlab.sh
+./evaluation/planetlab/setup/install.sh
--- evaluation/planetlab/setup/install.sh
+++ evaluation/planetlab/setup/install.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
 install_script=./evaluation/planetlab/setup/install_deps.sh
-servers=./config/servers.txt
+servers=$(cat ./config/servers.txt | sort | uniq)
+clients=$(cat ./config/clients.txt | sort | uniq)
 ssh_key=~/.ssh/planetlab_rsa
 slice=chalmersple_casss2
 
 failed=0
 
-while read server; do
+for server in $servers; do
   echo "Installing system on $server..."
   ssh -i $ssh_key -l $slice $server 'bash -s' < $install_script \
     && echo "Installation on $server sucessful!"  \
     || { echo "Installation on $server"; failed=1; break; }
-done <$servers
+done
+
+if [ $failed -eq 0 ]; then
+  for client in $clients; do
+    echo "Installing system on $client..."
+    ssh -i $ssh_key -l $slice $client 'bash -s' < $install_script \
+      && echo "Installation on $client sucessful!"  \
+      || { echo "Installation on $client"; failed=1; break; }
+  done
+fi
 
 if [ $failed -eq 0 ]; then
   echo "Installation sucessful!"
