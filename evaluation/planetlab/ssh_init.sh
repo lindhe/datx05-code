b2aec49d151c167285c1904ee6d56605d33653ae
Andreas Lindhé <andreas@lindhe.io>
2018-05-21 14:15:23 +0200

    Add script to check ssh login status

---
+++ evaluation/planetlab/ssh_init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+opts="StrictHostKeyChecking=no"
+slice=chalmersple_casss2
+all_things=$(cat ./config/servers.txt ./config/clients.txt | sort | uniq)
+
+echo "Removing clients results directory"
+
+for a in $all_things; do
+  echo "SSH to $a"
+  ssh -o $opts -l $slice -i ~/.ssh/planetlab_rsa $a "echo done"
+done
+
+echo "DONE"
