49a38f3da7a95074cb1130a76b59f44c41e9d5b3
Andreas Lindhé <andreas@lindhe.io>
2018-05-23 15:22:00 +0200

    If argument is given, start ar server

--- autostart.sh
+++ autostart.sh
@@ -12,6 +12,11 @@ servers=$(grep $address $config | cut -d ' ' -f 3)
 for server in $servers; do
   address=$(echo $server | cut -d ':' -f 1)
   port=$(echo $server | cut -d ':' -f 2)
-  echo "Starting server at $address:$port"
-  sudo python3.6 -O /home/$slice/casss/server.py $port $address $config > /dev/null &
+  if [ $# -eq 0 ]; then
+    echo "Starting server at $address:$port"
+    sudo python3.6 -O /home/$slice/casss/server.py $port $address $config > /dev/null &
+  else
+    echo "Starting AR server at $address:$port"
+    sudo python3.6 -O /home/$slice/casss/ARserver.py $port $address $config > /dev/null &
+  fi
 done
