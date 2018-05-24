cdf688fa227b09d3216bad085c2cecce4ac97ee9
Andreas Lindhé <andreas@lindhe.io>
2018-05-24 10:23:58 +0200

    Ignore __pycache__ properly

--- evaluation/planetlab/setup/copy_to_planetlab.sh
+++ evaluation/planetlab/setup/copy_to_planetlab.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+ig=config/rsync_ignore.txt
 opts="StrictHostKeyChecking=no"
 s=./config/servers.txt
 c=./config/clients.txt
@@ -8,12 +9,10 @@ ssh_key=~/.ssh/planetlab_rsa
 slice=chalmersple_casss2
 failed=0
 
-echo "Removing __pycache__ files..."
-find . -path "**/__pycache__/*" -delete
 echo "Copying working directory to servers..."
 
 for server in $all; do
-  rsync -rlptoPz --delete -e "ssh -o $opts -i $ssh_key -l $slice" ./* $server:~/casss \
+  rsync -rlptoPz --delete --exclude-from=$ig -e "ssh -o $opts -i $ssh_key -l $slice" ./* $server:~/casss \
     && echo "Transfer to $server sucessful!"  \
     || { echo "Failed transfer to $server"; failed=1; break; }
 done
