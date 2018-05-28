573159fb8bac0d2fe11c68c3421bb3b9cc9b49ed
Andreas Lindhé <andreas@lindhe.io>
2018-05-28 16:40:41 +0200

    Add zeromq to dependencies

--- evaluation/planetlab/setup/install_deps.sh
+++ evaluation/planetlab/setup/install_deps.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo dnf install -y python3-devel python36 redhat-rpm-config liberasurecode-devel python3-lxml
+sudo dnf install -y python3-devel python36 redhat-rpm-config liberasurecode-devel python3-lxml zeromq3-devel
 sudo python3.6 ~/casss/evaluation/planetlab/get-pip.py
 sudo pip3.6 install -r ~/casss/requirements.txt
