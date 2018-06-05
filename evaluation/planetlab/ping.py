624b2526f4111619c79c61ebc0b85b9ae6cfedf9
Andreas Lindhé <andreas@lindhe.io>
2018-06-05 17:43:13 +0200

    Round the numbers

--- evaluation/planetlab/ping.py
+++ evaluation/planetlab/ping.py
@@ -11,8 +11,8 @@ avg_list = []
 for addr in addr_list:
     response = os.popen("ping -c 10 "+addr+" | tail -1 | awk '{print $4}' | cut -d '/' -f 2").read()
     avg_list.append(float(response))
-max_val = max(avg_list)
-min_val = min(avg_list)
-avg_val = sum(avg_list)/float(len(avg_list))
+max_val = round(max(avg_list), 2)
+min_val = round(min(avg_list), 2)
+avg_val = round(sum(avg_list)/float(len(avg_list)), 2)
 print(f"min\t& max\t& avg")
 print(f"{min_val}\t& {max_val}\t& {avg_val}")
